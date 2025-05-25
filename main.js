548ed4b370041fbdd23ca8d1c2a4f36998dcadfa
xostrch <xostrch@icloud.com>
2025-05-25 17:28:53 +0200

    skonczona apka

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
     let taskIdCounter = 0;
+    let currentlyEditingTaskId = null;
 
     function closeError(){
         document.getElementById('error-box').style.display="none";
+    }
+
+    function closeEdit(){
+        document.getElementById('edit-box').style.display='none'
+    }
+
+    function editShow(taskNameId){
+        currentlyEditingTaskId = taskNameId;
+        document.getElementById('edit-box').style.display = 'block';
+        let currentText = document.getElementById(taskNameId).innerText.trim();
+        document.getElementById('input-edit').value = currentText;    
     }
 
 
@@ -16,6 +28,7 @@
             taskIdCounter++;
             let taskId = `task-${taskIdCounter}`;
             let taskNameId = `task-name-${taskIdCounter}`;
+            let taskEditId = `task-edit-${taskIdCounter}`
             let newTask =
                 `
                 <div class="task" id="${taskId}">
@@ -24,7 +37,7 @@
                     </div>
                     <div class="buttons">
                         <button class="button" onclick="doneTask('${taskNameId}')"><img src="done.png" class="img-icons"></button>
-                        <button class="button"><img src="edit.png" class="img-icons"></button>
+                        <button class="button" onclick="editShow('${taskNameId}')"><img src="edit.png" class="img-icons"></button>
                         <button class="button" onclick="deleteTask('${taskId}')"><img src="delete.png" class="img-icons"></button>
                     </div>
                 </div>`;
@@ -41,6 +54,24 @@
     }
 
     function doneTask(taskNameId){
-        let taskColor = document.getElementById(taskNameId);
-        taskColor.style.color="red";
+        let taskLine = document.getElementById(taskNameId);
+        if(taskLine.style.textDecoration==="line-through")
+        {
+            taskLine.style.textDecoration="none"
+        }
+        else
+        {
+            taskLine.style.textDecoration="line-through"
+        }
     }
+    
+
+    function editTask() {
+    let taskNewName = document.getElementById("input-edit").value;
+    if (currentlyEditingTaskId) {
+        document.getElementById(currentlyEditingTaskId).innerText = taskNewName;
+        currentlyEditingTaskId = null;
+        document.getElementById('edit-box').style.display = 'none';
+    }
+}
+
